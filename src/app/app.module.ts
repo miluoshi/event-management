1bf50c4f00cb3ad738f5088d0d0eeddfd3df87bf
Milos Lajtman <milos.lajtman@edocu.com>
2019-07-04 22:13:57 +0200

    Add @ngrx/{store,schematics,store-devtools} and create boilerplate for app store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { metaReducers, reducers } from './reducers';
 
 @NgModule({
   bootstrap: [AppComponent],
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    StoreModule.forRoot(reducers, { metaReducers }),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
+  ],
   providers: []
 })
 export class AppModule {}
---
+++ src/app/event.actions.ts
@@ -0,0 +1,11 @@
+import { Action } from '@ngrx/store';
+
+export enum EventActionTypes {
+  LoadEvents = '[Event] Load Events'
+}
+
+export class LoadEvents implements Action {
+  readonly type = EventActionTypes.LoadEvents;
+}
+
+export type EventActions = LoadEvents;
