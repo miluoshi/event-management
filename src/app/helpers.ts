6d084a5f1fb2e2987a3daf6e7d524fe4c06f61f3
Milos Lajtman <milos.lajtman@edocu.com>
2019-07-07 15:09:01 +0200

    Use faker.js to generate event info

--- src/app/helpers.ts
+++ src/app/helpers.ts
@@ -1,3 +1,4 @@
+import * as faker from 'faker';
 import { Event, EventMap } from './event-store/event.reducer';
 
 export const currentUser = 'demo';
@@ -20,19 +21,43 @@ export function normalizeEventsData(events: Event[]): EventMap {
  * starting from now and ending in 2 hours.
  */
 export function generateEvent(id: number, name: string): Event {
-  const timeStart = new Date();
-  const timeEnd = new Date(timeStart);
-  timeEnd.setHours(timeStart.getHours() + 2);
+  // Decide with equal probability whether event will be past/current/future
+  const timeBox = (['past', 'current', 'future'] as const)[Math.floor(Math.random() * 3)];
+  const { timeStart, timeEnd } = getRandomEventDates(timeBox);
+
+  // Randomly generate user ("demo" or random name)
+  const author = ['demo', faker.internet.userName()][Math.round(Math.random())];
 
   return {
-    author: currentUser,
-    description: 'random description',
+    author,
+    description: faker.lorem.paragraphs(2),
     id,
-    location: 'Bratislava',
+    location: faker.address.city(),
     name,
     timeEnd,
     timeStart
   };
+}
+
+function getRandomEventDates(timeBox: 'past' | 'current' | 'future') {
+  const randomEventDurationDays = faker.random.number({ min: 0, max: 10 });
+
+  let timeStart: Date;
+  let timeEnd: Date;
+  switch (timeBox) {
+    case 'past':
+      timeStart = faker.date.recent(randomEventDurationDays);
+      timeEnd = faker.date.between(timeStart, new Date());
+      break;
+    case 'current':
+      timeStart = faker.date.recent(randomEventDurationDays);
+      timeEnd = faker.date.soon(randomEventDurationDays);
+      break;
+    case 'future':
+      timeEnd = faker.date.soon(randomEventDurationDays);
+      timeStart = faker.date.between(new Date(), timeEnd);
+  }
+  return { timeStart, timeEnd };
 }
 
 /** Returns array of unique author ids from all events */
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import * as faker from 'faker';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
@@ -7,6 +8,10 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
+
+// Set "sk" locale for generated data
+// @ts-ignore
+faker.locale = 'sk';
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
